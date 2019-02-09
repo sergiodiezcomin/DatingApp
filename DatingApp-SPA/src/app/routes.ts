f591966fee7813eb83057cd3067615a03aa5c103
sergiodiezcomin <sergiodiez72@gmail.com>
2019-02-09 15:12:29 +0100

    End of Section 16

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -11,6 +11,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 
 
@@ -24,7 +25,7 @@ export const appRoutes: Routes = [
         { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
         { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
         { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
-        { path: 'messages', component: MessagesComponent},
+        { path: 'messages', component: MessagesComponent, resolve: {messages: MessagesResolver}},
         { path: 'lists', component: ListsComponent, resolve: {users: ListsResolver}}
       ]
     },
